47ab577d6a4b4d21fa530307d15f98a7a019af53
antiaim <activmcx@gmail.com>
2022-09-27 08:57:50 +0100

    Update savestate.js

--- savestate.js
+++ savestate.js
@@ -1,8 +1,59 @@
+
 "use strict"
 
-module.exports = createGLStateStack
+function unique_pred(list, compare) {
+  var ptr = 1
+    , len = list.length
+    , a = list[0], b = list[0]
+  for(var i=1; i<len; ++i) {
+    b = a
+    a = list[i]
+    if(compare(a, b)) {
+      if(i === ptr) {
+        ptr++
+        continue
+      }
+      list[ptr++] = a
+    }
+  }
+  list.length = ptr
+  return list
+}
 
-var uniq = require("uniq")
+function unique_eq(list) {
+  var ptr = 1
+    , len = list.length
+    , a = list[0], b = list[0]
+  for(var i=1; i<len; ++i) {
+    b = a
+    a = list[i]
+    if(a !== b) {
+      if(i === ptr) {
+        ptr++
+        continue
+      }
+      list[ptr++] = a
+    }
+  }
+  list.length = ptr
+  return list
+}
+
+function unique(list, compare, sorted) {
+  if(list.length === 0) {
+    return list
+  }
+  if(compare) {
+    if(!sorted) {
+      list.sort(compare)
+    }
+    return unique_pred(list, compare)
+  }
+  if(!sorted) {
+    list.sort()
+  }
+  return unique_eq(list)
+}
 
 function createGLStateStack(gl, variables) {
   //By default SAVE EVERYTHING
