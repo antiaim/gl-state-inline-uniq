4b96d6439bf0e896fd809241b7691fdaf5cc26ba
antiaim <activmcx@gmail.com>
2022-09-27 11:01:08 +0100

    Update savestate.js

--- savestate.js
+++ savestate.js
@@ -1,5 +1,4 @@
 "use strict"
-module.exports = createGLStateStack
 function unique_pred(list, compare) {
   var ptr = 1
     , len = list.length
@@ -395,4 +394,5 @@ main_loop:
   return proc(gl)
 }
 
+window.createGLStateStack = createGLStateStack
 
